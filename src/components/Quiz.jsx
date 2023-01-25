edc8715002b6ace08eb77d968e61c4c7fc413744
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-25 20:05:56 +0100

    Prapare quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  function onNext() {
+    console.log("next");
+  }
+  function onPrev() {
+    console.log("prev");
+  }
+  return (
+    <div className="container">
+      <h1 className="title">Quiz App</h1>
+
+      <div>
+        <button className="prev" onClick={onPrev}>
+          Prev
+        </button>
+        <button className="next" onClick={onNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
