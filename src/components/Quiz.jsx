8492be03ff440d96f7f0ba50a394ad6d927c3dd3
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-25 21:35:39 +0100

    Setup reducers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 import Questions from "./Questions";
 
+import { useSelector } from "react-redux";
+
 const Quiz = () => {
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    console.log(state);
+  }, []);
   function onNext() {
     console.log("next");
   }
